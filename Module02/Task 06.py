9f8b6a60b36ed792b0183d4e7aab14535701558a
Minaya Chanmini Gunawardhana Egodahewage <minayachanmini@outlook.com>
2025-08-21 21:17:15 +0530

    Update Task 06.py

--- Module02/Task 06.py
+++ Module02/Task 06.py
@@ -1,5 +1,5 @@
 import random
-code01 = [random.randint(0, 9) for _ in range (3)]
-code02 = [random.randint(1, 6) for _ in range (4)]
+code01 = [random.randint(0, 9) range (3)]
+code02 = [random.randint(1, 6) range (4)]
 print("code01:", code01)
 print("code02:", code02)
