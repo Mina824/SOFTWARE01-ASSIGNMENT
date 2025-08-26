466bae62dbc76b21e5def2200d2e2b966a1ec659
Minaya Chanmini Gunawardhana Egodahewage <minayachanmini@outlook.com>
2025-08-26 09:15:18 +0530

    Update Task 06.py

--- Module02/Task 06.py
+++ Module02/Task 06.py
@@ -1,5 +1,13 @@
 import random
-code01 = [random.randint(0, 9) range (3)]
-code02 = [random.randint(1, 6) range (4)]
-print("code01:", code01)
-print("code02:", code02)
+
+rand01 = random.randint(0,9)
+rand02 = random.randint(0,9)
+rand03 = random.randint(0,9)
+
+print(f"3_digit_code {rand01}{rand02}{rand03}")
+
+rand_num1 = random.randint(1,6)
+rand_num2 = random.randint(1,6)
+rand_num3 = random.randint(1,6)
+
+print(f"4_digit_code {rand_num1}{rand_num2}{rand_num3}")
