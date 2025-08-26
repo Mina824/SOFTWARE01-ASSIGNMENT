ee813a6727cd9680da3b7c7dbf9d598b37eb0818
Mina824 <minayachanmini@outlook.com>
2025-08-26 09:08:19 +0530

    completed task 06 of Module02

--- Module02/Task 06.py
+++ Module02/Task 06.py
@@ -1,5 +1,13 @@
 import random
-code01 = [random.randint(0, 9) for _ in range (3)]
-code02 = [random.randint(1, 6) for _ in range (4)]
-print("code01:", code01)
-print("code02:", code02)
+
+rand01 = random.randint(0,9)
+rand02 = random.randint(0,9)
+rand03 = random.randint(0,9)
+
+print(f"3_digit_code {rand01}{rand02}{rand03}")
+
+rand_num1 = random.randint(1,6)
+rand_num2 = random.randint(1,6)
+rand_num3 = random.randint(1,6)
+
+print(f"4_digit_code {rand_num1}{rand_num2}{rand_num3}")
