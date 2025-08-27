b6c1dc9ce65a14c13aa4529f66566a21a169da9a
Minaya Chanmini Gunawardhana Egodahewage <minayachanmini@outlook.com>
2025-08-27 21:01:15 +0530

    Delete Module04 directory

--- Module04/Task 01.py
+++
@@ -1,4 +0,0 @@
-coins = int(input("How many coins? "))
-while coins < 5:
-    print("Not enough! Give me {5-coins}")
-    coins = int(input("add more money "))
--- Module04/Task 02.py
+++
@@ -1,5 +0,0 @@
-correct_password = "2004"
-max_attempts = 3
-attempts = 0
-
-input
