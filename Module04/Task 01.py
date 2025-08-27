2aa8a814cbb47370ac66a2e4b56b4702caa6c67d
Mina824 <minayachanmini@outlook.com>
2025-08-27 20:32:25 +0530

    completed mod 03

---
+++ Module04/Task 01.py
@@ -0,0 +1,4 @@
+coins = int(input("How many coins? "))
+while coins < 5:
+    print("Not enough! Give me {5-coins}")
+    coins = int(input("add more money "))
---
+++ Module04/Task 02.py
@@ -0,0 +1,5 @@
+correct_password = "2004"
+max_attempts = 3
+attempts = 0
+
+input
